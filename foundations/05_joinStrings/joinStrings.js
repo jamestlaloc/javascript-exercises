126ea6b18b5a9e6ba799f94ad5fb947e65cc52bd
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-11 16:19:53 -0800

    Change strings to integers.

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -10,8 +10,8 @@
 // Add your code right below, good luck!
 const firstName = "Carlos";
 const lastName = "Stevenson";
-let thisYear = "1965";
-const birthYear = "1947";
+let thisYear = 1965;
+const birthYear = 1947;
 
 let greeting = "Hello! My name is " + firstName + " and I am" + thisYear - birthYear + " years old.";
 
