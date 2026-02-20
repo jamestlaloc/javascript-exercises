37573c6c66a8ae0b8c33d8278edf68682aec966f
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-20 14:06:53 -0800

    Write function adds numbers in-between args including args.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+// Takes two positive integers
+// Add every integer in-between, including the inputs
+// Return the sum
 
+const sumAll = function(n1, n2) {
+  let sum = 0;
+
+    for(let i = n1; i <= n2; i++) {
+      console.log(i);
+      sum += i;
+    }
+  return sum;
 };
 
 // Do not edit below this line
