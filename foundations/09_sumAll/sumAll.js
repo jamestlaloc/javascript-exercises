69551db1c106f82e3528eee5c640f6a125d91751
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-20 15:00:17 -0800

    Tough.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -4,13 +4,21 @@
 
 const sumAll = function(n1, n2) {
   let sum = 0;
-
-    for(let i = n1; i <= n2; i++) {
-      console.log(i);
+  
+    if(n1 < 0 || n2 < 0 || n1 === NaN || n2 === NaN || !Number.isInteger(n1) || !Number.isInteger(n2) ) {
+      return 'ERROR'
+    } else if(n1 <= n2) {
+      for(let i = n1; i <= n2; i++) {
       sum += i;
+      }
+    } else {
+      for(let i = n2; i <= n1; i++) {
+        sum += i;
+      }
     }
   return sum;
 };
+  
 
 // Do not edit below this line
 module.exports = sumAll;
