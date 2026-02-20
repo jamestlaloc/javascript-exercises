f10b962b5d3aaf144cd05dfe626820340f244e6b
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-20 11:22:51 -0800

    Write function that takes in 2 args and repeats string.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+// Take in a string and a number
+// If the number is negative, return 'ERROR'
+// Use loops to implement and not the built in repeat()
+
+const repeatString = function(str, num) {
+  if(num < 0) {
+    return 'ERROR'
+  } else {
+    for(let i = 0; i < num; i++) {
+       return str.concat(str, str)
+    }
+  }
 
 };
 
