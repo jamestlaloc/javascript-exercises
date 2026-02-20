b2448f029d7363bb5d3b8ced14d7375188c8124f
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-20 11:57:10 -0800

    Create empty string, add given string n times and assign.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -3,14 +3,17 @@
 // Use loops to implement and not the built in repeat()
 
 const repeatString = function(str, num) {
-  if(num < 0) {
+  let repeatedString = "";
+
+  if(num <+ 0) {
     return 'ERROR'
   } else {
     for(let i = 0; i < num; i++) {
-       return str.concat(str, str)
+      console.log(i)
+      repeatedString += str;
     }
   }
-
+  return repeatedString
 };
 
 // Do not edit below this line
