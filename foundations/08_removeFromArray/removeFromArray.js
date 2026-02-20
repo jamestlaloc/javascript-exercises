04171a3e81730c409515017d9059d23ee4c14d6a
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-20 13:37:12 -0800

    Write function, takes multiple args, removes args, from arr taken.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+// Take in an array and indefinite amount of args
+// Remove the other args from the array given
+// Return the resulting array
+
+const removeFromArray = function(arr, ...args) {
+  const index = arr.indexOf(...args);
+  
+  arr.splice(index, 1);
+  
+  return arr; 
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
