b468a83687fef88199d8514f13c5e60483353aea
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-21 02:58:35 -0800

    Cslculatooooor

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function(num) {
-	return num + num;
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = arr.reduce((total, currentNum) => total + currentNum, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = arr.reduce((total, currentNum) => total * currentNum, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  if(n < 0) {
+    return 'undefined'
+  } else if (n === 0 || n === 1){
+    return 1;
+  } else {
+    for(let i = 2; i <= n; i++) {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
