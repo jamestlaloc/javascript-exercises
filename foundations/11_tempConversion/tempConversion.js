fbafc2b91901d5af47934c98032e071ab8846cbf
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-20 22:57:50 -0800

    Getting better

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
 // Formula for Fahrenheit to Celsius: (32°F − 32) × 5/9 = 0°C
 
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const conversion = 5/9 * (f - 32);
+  const celsius = Math.round(conversion * 10) / 10;
+
+  return celsius;
 };
 
 
-//Formula for Celsius to Fahrenheit: 0°C × 9/5) + 32 = 32°F
-const convertToFahrenheit = function() {
+//Formula for Celsius to Fahrenheit: °F = (9/5 × °C) + 32
+const convertToFahrenheit = function(c) {
+  const conversion = (9/5 * c) + 32;
+  const fahrenheit = Math.round(conversion * 10) / 10;
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
