f77573871bcada24d50773bd50deb91dc92194b2
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-20 18:59:03 -0800

    Write function that determines if leap year or not.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+//Take in an integer
+//Determine if the integer is divisible by four
+//
 
+const leapYears = function(year) {
+  if(year % 4 === 0 || year % 400 === 0) {
+    return true;
+  } else if(year % 4 === 0 && year % 100) {
+    return false;
+  }
 };
 
 // Do not edit below this line
